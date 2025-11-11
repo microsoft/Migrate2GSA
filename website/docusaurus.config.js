71d4958d6456ea98c379eec326765680aad067b5
Andres Canello <39328890+andres-canello@users.noreply.github.com>
2025-11-11 12:00:13 +1000

    Docusaurus initial

---
+++ website/docusaurus.config.js
@@ -0,0 +1,158 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Migrate2GSA',
+  tagline: 'Accelate your migration to Global Secure Access',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://microsoft.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/migrate2gsa/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'microsoft', // Usually your GitHub org/user name.
+  projectName: 'migrate2gsa', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   feedOptions: {
+        //     type: ['rss', 'atom'],
+        //     xslt: true,
+        //   },
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        //   // Useful options to enforce blogging best practices
+        //   onInlineTags: 'warn',
+        //   onInlineAuthors: 'warn',
+        //   onUntruncatedBlogPosts: 'warn',
+        // },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        title: 'Migrate2GSA',
+        logo: {
+          alt: 'Migrate2GSA Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'docsSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+         // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/microsoft/migrate2gsa',
+            label: 'Microsoft',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Resources',
+            items: [
+              {
+                label: 'Resources',
+                to: '/',
+              },
+            ],
+          },
+          {
+            title: 'Quick Links',
+            items: [
+              {
+                label: 'Global Secure Access',
+                href: 'https://learn.microsoft.com/entra/global-secure-access',
+              },
+              {
+                label: 'Private Access',
+                href: 'https://learn.microsoft.com/entra/global-secure-access/concept-private-access',
+              },
+              {
+                label: 'Internet Access',
+                href: 'https://learn.microsoft.com/entra/global-secure-access/concept-internet-access',
+              },
+            ],
+          },
+          {
+            title: 'Have feedback? Open an issue in GitHub or email us',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/microsoft/GlobalSecureAccess',
+              },
+              {
+                label: 'Email us',
+                href: 'mailto:GSAPOC@microsoft.com',
+              }
+            ],
+          },
+        ],
+        copyright: `Built by the Microsoft Identity Customer Acceleration Team.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
